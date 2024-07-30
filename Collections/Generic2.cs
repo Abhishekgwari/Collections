252e1d94a2c39cc7326a22eecd3f6ff413ffdda7
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-30 22:13:15 +0530

    Generic Method, Class

---
+++ Collections/Generic2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Collections
+{
+    class Generic2<T>
+    {
+        public void Add(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 + d2);
+        }
+        public void Sub(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 - d2);
+        }
+        public void Mul(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 * d2);
+        }
+        public void Div(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 / d2);
+        }
+    }
+
+    class TestGenerics
+    {
+        static void Main(string[] args)
+        {
+            Generic2<int> obj = new Generic2<int>();
+            obj.Add(10, 20);
+            obj.Sub(10, 20);
+            obj.Mul(10, 20);
+            obj.Div(10, 20);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Collections/Generics1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Generics1                      //Generic method
+    {
+        public bool Compare<T>(T a,T b)
+        {
+            if(a.Equals(b))
+                return true;
+            return false;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Generics1 obj = new Generics1();
+             bool result = obj.Compare<float>(23.46f, 45.51f);
+            Console.WriteLine(result);
+
+            bool result2 = obj.Compare<int>(10, 10);
+            Console.WriteLine(result2);
+
+        }
+    }
+}
