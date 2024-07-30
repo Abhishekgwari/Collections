e29c583108b056550e2f4eb36d5fdc5034e4ed71
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-30 23:49:22 +0530

    Generic

---
+++ Collections/Customer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    internal class Customer
+    {
+        public int Custid { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public double Balance { get; set; }
+    }
+
+    class TestCustomer
+    {
+        static void Main(string[] args)
+        {
+            List<Customer> Customers = new List<Customer>();
+
+            Customer c1 = new Customer
+            {
+                Custid = 1,
+                Name = "Abhishek",
+                City = "Delhi",
+                Balance = 34440.00
+            };
+
+            Customer c2 = new Customer
+            {
+                Custid = 2,
+                Name = "Aman",
+                City = "Bangalore",
+                Balance = 43000.00
+            };
+
+            Customer c3 = new Customer
+            {
+                Custid = 3,
+                Name = "Neha",
+                City = "Uttar Pradesh",
+                Balance = 54000.00
+            };
+
+            Customer c4 = new Customer
+            {
+                Custid = 4,
+                Name = "Lokesh",
+                City = "Faridabad",
+                Balance = 29000.00
+            };
+
+            Customers.Add(c1);
+            Customers.Add(c2);
+            Customers.Add(c3);
+            Customers.Add(c4);
+
+            foreach (Customer obj in Customers)
+            {
+                Console.WriteLine($"{obj.Custid} {obj.Name} {obj.City} {obj.Balance}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Collections/DictionaryCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class DictionaryCollection
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, object> dt = new Dictionary<string, object>();
+            dt.Add("Eid", 1);
+            dt.Add("Ename", "Abhishek");
+            dt.Add("Job", "Daveloper");
+            dt.Add("Salary", 40000);
+            dt.Add("Mgrid", 1003);
+            dt.Add("Phone", "564546645");
+            dt.Add("Email", "abc@gmail.com");
+            dt.Add("Dname", "IT");
+            dt.Add("Location", "Mumbai");
+            dt.Add("Did", 30);
+
+            foreach (var key in dt.Keys)
+            
+
+                Console.WriteLine(key + " :" + dt[key]);
+                Console.ReadLine();
+            
+
+        }
+    }
+}
