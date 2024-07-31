1066e457d750ff299d1f0cf2f88a79dc926eef0f
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-31 16:32:29 +0530

    Collection (Sorting using Comparision Delegates).

--- Collections/Student.cs
+++ Collections/Student.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
                                       
 namespace Collections                                // Icomparable Interface
 {
-    internal class Student:IComparable<Student>
+    internal class Student :IComparable<Student>
 
     {
-        public int Sid {  get; set; }
+        public int Sid { get; set; }
         public string Name { get; set; }
-        public int Class {  get; set; }
+        public int Class { get; set; }
         public float Marks { get; set; }
         public int CompareTo(Student other)
         {
@@ -20,24 +20,29 @@ namespace Collections                                // Icomparable Interface
             else if (this.Sid < other.Sid)
                 return -1;
             else
-            
+
                 return 0;
         }
+    
         class CompareStudents : IComparer<Student>           // I comparer interface
         {
             public int Compare(Student x, Student y)
             {
-               if(x.Marks > y.Marks)
+                if (x.Marks > y.Marks)
                     return 1;
-               else if (x.Marks < y.Marks)
+                else if (x.Marks < y.Marks)
                     return -1;
-               else 
+                else
                     return 0;
             }
         }
 
         class TestStudent
         {
+            //public static int CompareNames(Student S1, Student S2)
+            //{
+            //   return  S1.Name.CompareTo(S2.Name);
+            //}
 
             static void Main(string[] args)
             {
@@ -49,10 +54,28 @@ namespace Collections                                // Icomparable Interface
                 Student s6 = new Student { Sid = 64, Name = "Rachit", Class = 4, Marks = 80.90f };
 
                 List<Student> Students = new List<Student>() { s1, s2, s3, s4, s5, s6, };
-                CompareStudents obj = new CompareStudents();
-                Students.Sort(obj);
+                //  Students.Sort();
+                //  CompareStudents obj = new CompareStudents();
+                // Students.Sort(obj);
 
                 //  Students.Reverse();
+
+                //    Students.Sort(1,5,obj);
+
+                // option 1
+                //   Comparison<Student> obj = new  Comparison<Student>
+                //     (CompareNames);
+                // Students.sort(obj);
+
+
+                // option 2
+                //     Students.Sort(obj);
+
+                // option 3         //Students.Sort(delegate(Student S1 , Student S2) { 
+                //    return S1.Name.CompareTo(S2.Name); });
+
+                // option 4
+                Students.Sort((S1, S2) => S1.Name.CompareTo(S2.Name));
 
                 foreach (Student S in Students)
                 {
@@ -60,7 +83,7 @@ namespace Collections                                // Icomparable Interface
                 }
                 Console.ReadLine();
             }
-
+        }
         }
     }
-    }
+    
